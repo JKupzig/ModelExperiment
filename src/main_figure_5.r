e6a9f5ca86b4314b87a949d1c830fd2946df4b05
JKupzig <jenny.kupzig@posteo.de>
2025-02-03 16:33:25 +0100

    change text size im image

--- src/main_figure_5.r
+++ src/main_figure_5.r
@@ -32,7 +32,7 @@ data_to_plot <- data %>%
   tidyr::pivot_longer(!date)
 
 ggplot(data_to_plot) +
-  geom_line(aes(x = date, y = value, col = name)) +
+  geom_line(aes(x = date, y = value, col = name), lwd=1.) +
   scale_x_date(date_labels = "%b %Y") +
   xlim(zoom_1_x) +
   ylim(zoom_1_y) +
@@ -46,16 +46,17 @@ ggplot(data_to_plot) +
     labels=c("as_lakes" = "reservoir algorithm (V0)",
             "schneider" = "reservoir algorithm (V2)")) +
     theme(legend.position = c(0.4, 0.9),
-          legend.text = element_text(colour="black", size=14)
-          )
+          legend.text = element_text(colour="black", size=14),
+          text = element_text(size = 14),
+          axis.text.x = element_text(size=14))
 
 
 ggsave(plot_name_1,
-       units = "cm", width = 16, height = 14, dpi = 300)
+       units = "cm", width = 20, height = 14, dpi = 300)
 
 
 ggplot(data_to_plot) +
-  geom_line(aes(x = date, y = value, col = name)) +
+  geom_line(aes(x = date, y = value, col = name), lwd=1.) +
   scale_x_date(date_labels = "%b %Y") +
   xlim(zoom_2_x) +
   ylim(zoom_2_y) +
@@ -67,7 +68,8 @@ ggplot(data_to_plot) +
     values = c("schneider" = datylon_map[2],
                 "as_lakes" = datylon_map[5])) +
     theme(legend.position = "none",
-    legend.text = element_text(colour="black", size=14))
+          text = element_text(size = 14),
+          axis.text.x = element_text(size=14, angle = 45, hjust=1))
 
 ggsave(plot_name_2,
        units = "cm", width = 10, height = 8, dpi = 300)
