8380246cbc6584a669ca1fdae73755884d5e536d
JKupzig <jenny.kupzig@posteo.de>
2025-02-03 09:39:12 +0100

    delete unused code in helper functions

--- src/helper/comparison.r
+++ src/helper/comparison.r
@@ -1,41 +1,3 @@
-compare_models_reservoirs <- function(cal_results,
-                                      reference_column,
-                                      to_compare_column_1,
-                                      to_compare_column_2,
-                                      label_to_use,
-                                      min_quality = NULL){
-
-
-  station_sufficient_quality <- cal_results$station
-
-  if (!is.null(min_quality)) {
-    station_sufficient_quality <- cal_results$station[
-      cal_results[reference_column] > min_quality |
-        cal_results[to_compare_column_1] > min_quality |
-        cal_results[to_compare_column_2] > min_quality]
-  }
-
-
-  selected_stations <- cal_results$station %in% station_sufficient_quality
-  cal_results_intersting <- cal_results[selected_stations,]
-
-  delta_1 <- cal_results_intersting[[to_compare_column_1]]  -
-    cal_results_intersting[[reference_column]]
-
-  delta_2 <- cal_results_intersting[[to_compare_column_2]]  -
-    cal_results_intersting[[reference_column]]
-
-  data_1 <- data.frame("value"= delta_1,
-                       "label"= paste0(label_to_use, " schneider"),
-                       "basin_id"= cal_results_intersting$station)
-
-  data_2 <- data.frame("value"= delta_2,
-                       "label"= paste0(label_to_use, " hanasaki"),
-                       "basin_id"= cal_results_intersting$station)
-
-  return(rbind(data_1, data_2))
-}
-
 
 compare_models <- function(cal_results,
                            reference_column,
@@ -52,16 +14,15 @@ compare_models <- function(cal_results,
   }
 
   selected_stations <- cal_results$station %in% c(station_sufficient_quality)
-  cal_results_intersting <- cal_results[selected_stations,]
-  cal_results_others <- cal_results[!selected_stations,]
+  cal_results_interesting <- cal_results[selected_stations, ]
 
   # positive --> alternative better than reference!
-  delta <- (cal_results_intersting[[to_compare_column]]  -
-              cal_results_intersting[[reference_column]])
+  delta <- (cal_results_interesting[[to_compare_column]]  -
+              cal_results_interesting[[reference_column]])
 
-  data <- data.frame("value"= delta,
-                     "label"= label_to_use,
-                     "basin_id"=cal_results_intersting$station)
+  data <- data.frame("value" = delta,
+                     "label" = label_to_use,
+                     "basin_id" =cal_results_interesting$station)
 
   return(data)
 }
--- src/helper/modify_run.r
+++
@@ -1,9 +0,0 @@
-modify_run <- function(basin_run, settings)
-{
-  new_run <- basin_run;
-  for (name in names(settings))
-  {
-    new_run[[name]] <- settings[[name]]
-  }
-  return(new_run)
-}
