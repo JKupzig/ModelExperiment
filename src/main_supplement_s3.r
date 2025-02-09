123c3eb437e47d4c7e4883042b3c4395e75cf652
JKupzig <jenny.kupzig@posteo.de>
2025-02-09 08:50:36 +0100

    fix bug in uncertainty and enhance plot

--- src/main_supplement_s3.r
+++ src/main_supplement_s3.r
@@ -6,15 +6,16 @@ source("./src/helper/read_data.r")
 
 # TODO update uncertainty data, for some basins not the calibrated gamma value where used in model runs!
 ROOT <- "./data/cal_result_model_m%i_uncertainty.rds"
-TARGET <- "./plots/supplement/supplement_s3_%s_%s.png"
+TARGET <- "./plots/supplement/supplement_s3_%s.png"
 
 REFERENCE_MODEL <- c(8, 8, 8, 8)
 COMPARISON_MODEL <- c(16, 12, 11, 9)
 GROUP_NAMES <- c("schneider", "hanasaki", "variable", "no snow")
-LABELS <- c("reference",
-            "x_p", "+x_hyd", "-x_hyd", "x_p and +x_hyd", "x_p and -x_hyd")
+LABELS <- c("original",
+            expression(x[p]), expression(+x[hyd]), expression(-x[hyd]),
+            expression(paste(x[p], " & ", +x[hyd])), expression(paste(x[p], " & ", -x[hyd])))
 
-behavioural_basins <- read_kge_and_define_good_basins(NULL, NULL)
+behavioural_basins <- read_kge_and_define_good_basins(0.0, NULL)
 
 for (comparison_id in seq_along(GROUP_NAMES)){
 
@@ -25,8 +26,8 @@ for (comparison_id in seq_along(GROUP_NAMES)){
   stations_to_use <- which(dimnames(uncertain_KGE_best)[[4]] %in% behavioural_basins$behavioural)
   benchmark_to_use <- which(dimnames(uncertain_KGE_best)[[2]] == benchmark_to_examine)
 
-  #png(sprintf(TARGET, GROUP_NAMES[comparison_id], name),
-  #    res = 300, units = "cm", width = 16, height = 12)
+  png(sprintf(TARGET, GROUP_NAMES[comparison_id]),
+      res = 300, units = "cm", width = 16, height = 12)
   par(mfrow = c(1, 6), mar = c(5, 2, 3, 2) + .1)
 
   interesting_ids <- NULL
@@ -49,15 +50,23 @@ for (comparison_id in seq_along(GROUP_NAMES)){
     boxplot(list(
       "delta" = standard_run[interesting_ids] - recommended_run[interesting_ids]),
             ylim = c(-0.5, 0.5),
-            xlab = LABELS[uncertainty_id],
-            ylab("\u0394KGE"))
+            xlab = LABELS[uncertainty_id])
 
     if (uncertainty_id == 1) {
       title(main = sprintf("number of basins: %i", length(interesting_ids)),
-            cex.main = 0.7)
+            cex.main = 0.7,
+            ylab ="\u0394KGE")
     }
 
     abline(h = 0, col = "firebrick")
   }
-  #dev.off()
+  dev.off()
 }
+
+# verprobung:
+# ROOT1 <- "./data/cal_result_model_m8_uncertainty.rds"
+# ROOT2 <- "./data/cal_result_benchmarks_model_m8_wetlStorage100.txt"
+# new_data <- readRDS(ROOT1)
+# ref_data <- read.csv(ROOT2, sep = "\t")
+# sum(new_data[2,1,1, ] == ref_data$KGE_val) / length(ref_data$KGE_val) #100 %
+
